ce01b122cfee50d11795d14b398941b2e112e23c
Natalia Gribanova <kara333@mail.ru>
2024-05-24 19:53:26 +0300

    task 3 part 1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Phone(models.Model):
@@ -7,4 +8,7 @@ class Phone(models.Model):
     image = models.URLField()
     release_date = models.DateField()
     lte_exists = models.BooleanField()
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
+
+    def get_absolute_url(self):
+        return reverse('phone', kwargs={'slug': self.slug})
